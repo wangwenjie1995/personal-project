713a387e19d0e3dcef6c3c6e1e53e82d3771e02c
wagnwenjie <wagnwenjie1995@gmail.com>
2019-09-12 02:47:16 +0800

    test

--- src/views/dashboard/index.vue
+++ src/views/dashboard/index.vue
@@ -1,18 +1,21 @@
 <template>
   <div class="dashboard-container">
-    <iframe src="https://wangwenjie1995.github.io/wangwenjie.github.io/wangwenjie.pdf" style="width: 100%;height:60px;" scrolling="no" type="application/pdf">
+    <iframe src="https://docs.google.com/document/d/e/2PACX-1vRpPLw6K6huEHC1WX4YPPVpd1Yu67747BhTydMtgJHqiQGiVDtl4RA8m0SRTGxi2q8VSDxyeFYS2Ziv/pub?embedded=true">
+      您的浏览器不支持PDF阅读
+    </iframe>
+    <!-- <iframe src="https://wangwenjie1995.github.io/wangwenjie.github.io/wangwenjie.pdf" style="width: 100%;height:60px;" scrolling="no" type="application/pdf">
       您的浏览器不支持PDF阅读
       <a href="https://wangwenjie1995.github.io/wangwenjie.github.io/wangwenjie.pdf">Download PDF</a>
-    </iframe>
+    </iframe> -->
 
-    <object data="https://wangwenjie1995.github.io/wangwenjie.github.io/wangwenjie.pdf" type="application/pdf" width="100%" height="20%">
+    <!-- <object data="https://wangwenjie1995.github.io/wangwenjie.github.io/wangwenjie.pdf" type="application/pdf" width="100%" height="20%">
       This browser does not support PDFs. Please download the PDF to view it: <a href="https://wangwenjie1995.github.io/wangwenjie.github.io/wangwenjie.pdf">Download PDF</a>
-    </object>
+    </object> -->
     <!-- why bottom: 3px; 使用embed打开pdf,设置height:100%; 高度多了3px bug -->
     <!-- 移动端不支持embed显示pdf -->
-    <div style="position: absolute; top: 60px; right: 0; bottom: 3px; left: 0;">
+    <!-- <div style="position: absolute; top: 60px; right: 0; bottom: 3px; left: 0;">
       <embed src="https://wangwenjie1995.github.io/wangwenjie.github.io/wangwenjie.pdf" name="plugin" width="100%" height="100%" type="application/pdf">
-    </div>
+    </div> -->
   </div>
 </template>
 

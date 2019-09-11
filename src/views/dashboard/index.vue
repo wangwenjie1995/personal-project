2b971f0945034e7cf873e1c1b9d296e9f5c1fe38
wagnwenjie <wagnwenjie1995@gmail.com>
2019-09-12 03:19:40 +0800

    test

--- src/views/dashboard/index.vue
+++ src/views/dashboard/index.vue
@@ -1,13 +1,16 @@
 <template>
   <div class="dashboard-container">
-    <iframe src="https://wangwenjie1995.github.io/wangwenjie.github.io/wangwenjie.pdf">
-      您的浏览器不支持PDF阅读
-    </iframe>
     <!-- <iframe src="https://wangwenjie1995.github.io/wangwenjie.github.io/wangwenjie.pdf" style="width: 100%;height:60px;" scrolling="no" type="application/pdf">
       您的浏览器不支持PDF阅读
       <a href="https://wangwenjie1995.github.io/wangwenjie.github.io/wangwenjie.pdf">Download PDF</a>
     </iframe> -->
-
+    <iframe src="https://wangwenjie1995.github.io/wangwenjie.github.io/wangwenjie.pdf" style="width: 100%;height:100%;" scrolling="no" type="application/pdf">
+      您的浏览器不支持PDF阅读
+      <a href="https://docs.google.com/document/d/1vzGVIal25Rhc0ip5ywQfqznwPU-uaRtAwZr7PqCoHEA/edit?usp=sharing">Download PDF</a>
+    </iframe>
+    <!-- <object data="https://docs.google.com/document/d/1vzGVIal25Rhc0ip5ywQfqznwPU-uaRtAwZr7PqCoHEA/edit?usp=sharing">
+      <embed src="https://docs.google.com/document/d/1vzGVIal25Rhc0ip5ywQfqznwPU-uaRtAwZr7PqCoHEA/edit?usp=sharing" type="application/pdf">
+    </object> -->
     <!-- <object data="https://wangwenjie1995.github.io/wangwenjie.github.io/wangwenjie.pdf" type="application/pdf" width="100%" height="20%">
       This browser does not support PDFs. Please download the PDF to view it: <a href="https://wangwenjie1995.github.io/wangwenjie.github.io/wangwenjie.pdf">Download PDF</a>
     </object> -->
